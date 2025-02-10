8b56fe541eb963c6c327ff088431646947181ae5
akbaradiw <akbaradiw000@gmail.com>
2025-02-10 11:09:59 +0700

    edit add to cart testing

--- tests/actions/addToCartActions.js
+++ tests/actions/addToCartActions.js
@@ -2,7 +2,7 @@ import addToCartLocator from "../locator/addToCartLocator";
 import authLocator from "../locator/authLocator";
 import { expect } from "@playwright/test";  
 
-export default class addToCartActions {
+export default class AddToCartActions {
   /**
    *
    * @param {import('@playwright/test').Page} page
@@ -12,60 +12,53 @@ export default class addToCartActions {
     this.page = page;
     this.addToCartLocator = new addToCartLocator(page);
     this.authLocator = new authLocator(page);
-    this.addSauceLabsBackpack = page.locator(
-      this.addToCartLocator.addSauceLabsBackpack
-    );
-    this.addLabsBikeLight = page.locator(
-      this.addToCartLocator.addLabsBikeLight
-    );
-    this.addLabsBoltTShirt = page.locator(
-      this.addToCartLocator.addLabsBoltTShirt
-    );
-    this.addLabsFleeceJacket = page.locator(
-      this.addToCartLocator.addLabsFleeceJacket
-    );
-    this.addLabsOnesie = page.locator(this.addToCartLocator.addLabsOnesie);
-    this.addLabsTestAllTheThingsTShirt = page.locator(
-      this.addToCartLocator.addLabsTestAllTheThingsTShirt
-    );
-    this.viewCart = page.locator(this.addToCartLocator.viewCart);
-    this.inputUsername = page.locator(this.authLocator.inputUsername);
-    this.inputPassword = page.locator(this.authLocator.inputPassword);
-    this.buttonLogin = page.locator(this.authLocator.buttonLogin);
+    
+    this.locators = {
+      addSauceLabsBackpack: page.locator(this.addToCartLocator.addSauceLabsBackpack),
+      addLabsBikeLight: page.locator(this.addToCartLocator.addLabsBikeLight),
+      addLabsBoltTShirt: page.locator(this.addToCartLocator.addLabsBoltTShirt),
+      addLabsFleeceJacket: page.locator(this.addToCartLocator.addLabsFleeceJacket),
+      addLabsOnesie: page.locator(this.addToCartLocator.addLabsOnesie),
+      addLabsTestAllTheThingsTShirt: page.locator(this.addToCartLocator.addLabsTestAllTheThingsTShirt),
+      viewCart: page.locator(this.addToCartLocator.viewCart),
+      inputUsername: page.locator(this.authLocator.inputUsername),
+      inputPassword: page.locator(this.authLocator.inputPassword),
+      buttonLogin: page.locator(this.authLocator.buttonLogin),
+    };
   }
 
-  async login(){
-    await this.page.goto('https://www.saucedemo.com/');
-    await this.inputUsername.fill('standard_user');
-    await this.inputPassword.fill ('secret_sauce');
-    await this.buttonLogin.click();
-}
-
-  async addSauceLabsBackpack() {
-    await this.addSauceLabsBackpack.click();
+  async login(username = "standard_user", password = "secret_sauce") {
+    await this.page.goto("https://www.saucedemo.com/");
+    await this.locators.inputUsername.fill(username);
+    await this.locators.inputPassword.fill(password);
+    await this.locators.buttonLogin.click();
   }
 
-  async addLabsBikeLight() {
-    await this.addLabsBikeLight.click();
+  async clickSauceLabsBackpack() {
+    await this.locators.addSauceLabsBackpack.click();
   }
 
-  async addLabsBoltTShirt() {
-    await this.addLabsBoltTShirt.click();
+  async clickLabsBikeLight() {
+    await this.locators.addLabsBikeLight.click();
   }
 
-  async addLabsFleeceJacket() {
-    await this.addLabsFleeceJacket.click();
+  async clickLabsBoltTShirt() {
+    await this.locators.addLabsBoltTShirt.click();
   }
 
-  async addLabsOnesie() {
-    await this.addLabsOnesie.click();
+  async clickLabsFleeceJacket() {
+    await this.locators.addLabsFleeceJacket.click();
   }
 
-  async addLabsTestAllTheThingsTShirt() {
-    await this.addLabsTestAllTheThingsTShirt.click();
+  async clickLabsOnesie() {
+    await this.locators.addLabsOnesie.click();
   }
 
-  async viewCart() {
-    await this.viewCart.click();
+  async clickLabsTestAllTheThingsTShirt() {
+    await this.locators.addLabsTestAllTheThingsTShirt.click();
+  }
+
+  async clickCart() {
+    await this.locators.viewCart.click();
   }
 }
--- tests/assertions/addToCart.spec.js
+++ tests/assertions/addToCart.spec.js
@@ -1,5 +1,4 @@
 const { test, expect } = require("@playwright/test");
-// const { default: authActions } = require("../../tests/actions/authActions");
 const { default: addToCartActions } = require("../../tests/actions/addToCartActions");
 
 test.describe("Add to cart", () => {
@@ -11,37 +10,37 @@ test.describe("Add to cart", () => {
   });
 
   test("Add 1 item to cart", async () => {
-    // await objActions.addSauceLabsBackpack();
-    await objActions.viewCart();
+    await objActions.clickSauceLabsBackpack();    
+    await objActions.clickCart();
   });
 
   test("Add 2 items to cart", async () => {
-    await objActions.addSauceLabsBackpack();
-    await objActions.addSauceLabsBoltTShirt();
-    await objActions.viewCart();
+    await objActions.clickSauceLabsBackpack();
+    await objActions.clickLabsBikeLight();
+    await objActions.clickCart();
   });
 
   test("Add 3 items to cart", async () => {
-    await objActions.addSauceLabsBackpack();
-    await objActions.addSauceLabsBoltTShirt();
-    await objActions.addSauceLabsFleeceJacket();
-    await objActions.viewCart();
+    await objActions.clickSauceLabsBackpack();
+    await objActions.clickLabsBikeLight();
+    await objActions.clickLabsBoltTShirt();
+    await objActions.clickCart();
   });
 
   test("Add 4 items to cart", async () => {
-    await objActions.addSauceLabsBackpack();
-    await objActions.addSauceLabsBoltTShirt();
-    await objActions.addSauceLabsFleeceJacket();
-    await objActions.addSauceLabsOnesie();
-    await objActions.viewCart();
+    await objActions.clickSauceLabsBackpack();
+    await objActions.clickLabsBikeLight();
+    await objActions.clickLabsBoltTShirt();    
+    await objActions.clickLabsFleeceJacket();
+    await objActions.clickCart();
   });
 
   test("Add 5 items to cart", async () => {
-    await objActions.addSauceLabsBackpack();
-    await objActions.addSauceLabsBoltTShirt();
-    await objActions.addSauceLabsFleeceJacket();
-    await objActions.addSauceLabsOnesie();
-    await objActions.addSauceLabsTestAllTheThingsTShirt();
-    await objActions.viewCart();
+    await objActions.clickSauceLabsBackpack();
+    await objActions.clickLabsBikeLight();
+    await objActions.clickLabsBoltTShirt();    
+    await objActions.clickLabsFleeceJacket();
+    await objActions. clickLabsOnesie();
+    await objActions.clickCart();
   });
 });
